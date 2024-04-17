2044ef3f6a58c98e6c030d6a82a1fa6a7143def6
tarou-jp <itohisahideki@gmail.com>
2024-04-17 21:19:45 +0900

    ✨ footerの作成、topページでのみ適応されるlayout作成

---
+++ front/layouts/darkdefault.vue
@@ -0,0 +1,27 @@
+<template>
+  <common-header />
+  <div class="page">
+    <slot />
+  </div>
+  <common-footer />
+</template>
+
+<script>
+import CommonHeader from '~/components/bases/header'
+import CommonFooter from '~/components/bases/darkFooter'
+
+export default {
+  components: { CommonHeader, CommonFooter },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.page {
+  margin-top: 60px;
+}
+</style>

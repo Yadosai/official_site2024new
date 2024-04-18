2140eda4ae2e6c05ccaa8c9b94b574e17846b7d8
tarou-jp <itohisahideki@gmail.com>
2024-04-18 22:40:56 +0900

    ✨ 新歓ページの実装

--- front/components/sinkan/index.vue
+++ front/components/sinkan/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div id="eyeCatch">
+    <div id="SinkanEyeCatch">
         <p>新歓情報</p>
         <img src="~/assets/img/sinkan/calendar.png">
     </div>
     <div id="SNSInjection">
         <div class="InjectionItem">
             <p>インスタ注目投稿</p>
-            <!-- <iframe src="https://www.instagram.com/p/C5ao9lIPW_E/embed" class="InjectionItemFrame" frameborder="0"
+            <iframe src="https://www.instagram.com/p/C5ao9lIPW_E/embed" class="InjectionItemFrame" frameborder="0"
                 scrolling="no" allowtransparency="true">
-            </iframe> -->
+            </iframe>
         </div>
     </div>
 </template>
@@ -17,7 +17,7 @@
 </script>
 
 <style lang="scss">
-#eyeCatch {
+#SinkanEyeCatch {
     display: flex;
     width: calc(100vw - 50px);
     height: auto;
@@ -29,7 +29,7 @@
 }
 
 .InjectionItem p,
-#eyeCatch p {
+#SinkanEyeCatch p {
     font-family: map-get($font-styles, family);
     font-style: map-get($font-styles, style);
     font-weight: map-get($font-styles, weight);
@@ -39,7 +39,7 @@
     margin: 0px;
 }
 
-#eyeCatch img {
+#SinkanEyeCatch img {
     width: 70%;
     height: auto;
 }

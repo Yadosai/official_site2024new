336df23a1cd031cd005df0435c8f1ff5fcf5e9c9
nowex35 <a.hayate0726@gmail.com>
2024-04-21 19:48:11 +0900

    コメント部分を修正

--- front/components/thanks/sponsorItem.vue
+++ front/components/thanks/sponsorItem.vue
@@ -21,36 +21,42 @@ export default {
 }
 </script>
   
-<style scoped>
+<style lang="scss">
 .sponsor {
-    height: 160px; /* 高さを160pxに設定 */
-    width: 100%; /* 幅を画面幅全体に設定 */
+    height: 160px;
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: rgba(255,255,255,1); /* 背景色を設定、オプション */
+    background-color: rgba(255,255,255,1);
 }
 
 .sponsor-logo-container {
-    width: 101px; /* コンテナの幅を100%に設定 */
-    height: 70px; /* コンテナの高さも親要素に合わせて100%に設定 */
+    width: 101px;
+    height: 70px;
+    display: flex;
+    align-items: center;
 }
 
 .sponsor-logo {
-    max-height: 100%; /* ロゴの最大高さをコンテナの高さに合わせる */
+    max-height: 100%;
     object-fit: contain; /* アスペクト比を保ちながら最大限に表示 */
 }
 
 .sponsor-descript {
+    font-style: map-get($font-styles, style);
+    font-weight: map-get($font-styles, weight);
+    line-height: map-get($font-styles, line-height);
+    font-family: map-get($font-styles, family);
     text-align: center;
     font-size: 14px;
-    width: 100%; /* 名前部分も幅いっぱいに展開 */
-    height: auto; /* 高さは自動で */
+    width: 100%;
+    height: auto;
     white-space: pre-wrap; /* 改行と空白を保持 */
 }
 
 .no-underline {
-    text-decoration: none; /* リンクの下線を消す */
-    color: black; /* リンクの色を黒に設定 */
+    text-decoration: none;
+    color: black;
 }
 </style>

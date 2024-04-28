b57a2df1ae1984d15f9b9c8e9f47c4e5cf70102e
nowex35 <a.hayate0726@gmail.com>
2024-04-28 22:08:20 +0900

    publicに移設

--- front/components/thanks/index.vue
+++ front/components/thanks/index.vue
@@ -1,18 +1,16 @@
 <template>
     <h1>協賛ページコンポーネント</h1>
-    <sponsorItem sponsorUrl="https://www.seirock-ya.jp/" :sponsorLogoUrl="SeirokuyaImg" :sponsorDescript="SeirokuyaText"  />
+    <sponsorItem sponsorUrl="https://www.seirock-ya.jp/" sponsorLogoUrl="/img/seirokuya.png" :sponsorDescript="SeirokuyaText"/>
 </template>
   
 <script>
 import sponsorItem from '~/components/thanks/sponsorItem'
-import Seirokuya from '~/assets/img/sponsor/seirokuya.png'
 
 
 export default {
     components: { sponsorItem },
     data() {
         return {
-            SeirokuyaImg: Seirokuya,
             SeirokuyaText: "我々は清六家。\n日本一のラーメン屋..."
         };
     }

b95709858f1bf3fb58a6b19e53666f1c41f0f62e
nowex35 <a.hayate0726@gmail.com>
2024-04-28 22:44:18 +0900

    コンポーネントにします

---
+++ front/components/comingsoon/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="ComingSoon">
+            <h1 class="CSTitle">Coming Soon!</h1>
+            <p class="CSdescription">このページは現在準備中です。</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+.ComingSoon {
+    text-align: center;
+    font-family: map-get($font-styles, family);
+    font-style: map-get($font-styles, style);
+    font-weight: map-get($font-styles, weight);
+    line-height: map-get($font-styles, line-height);
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-color: $quaternary-color;
+}
+
+
+
+</style>
